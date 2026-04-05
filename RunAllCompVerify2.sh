4f5e824cde0a51bb7b2ba98a2338f7578600178c
SLowry <lowryst@mail.uc.edu>
2026-04-05 01:43:30 -0400

    Ran all tests with memory logs

--- RunAllCompVerify2.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/time -v python3 ./Lowry_Clishe_CompVerify.py Tautology-Checking-Tests/TC_T8 Lowry_Clishe_Complement_Covers/TC_T8_compl 2>&1 | tee ./Lowry_Clishe_Complement_Covers/Logs/TC_T8_CompVerify.txt
